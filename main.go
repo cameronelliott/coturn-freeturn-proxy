59224a839365dab364127901b2334355fac3ec7a
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2020-05-20 18:50:20 -0700

    deployment testing

--- main.go
+++ main.go
@@ -233,7 +233,7 @@ func main() {
 
 		
     
-    err = ioutil.WriteFile("/tmp/foo", []byte("foo13"), 0644)
+    err = ioutil.WriteFile("/tmp/foo", []byte("foo14"), 0644)
 	check(err)
 	
 	lj := &lumberjack.Logger{
